33a9cb430cf03c9af5d91e95c528d8790bb33076
Michał <gry2@vp.pl>
2025-05-11 19:30:29 +0200

    Closes #5: Handle NFC intent with custom mimeType

--- app/src/main/java/pl/mikom/drynomore/feature/nfc/ui/NfcRedirectActivity.kt
+++ app/src/main/java/pl/mikom/drynomore/feature/nfc/ui/NfcRedirectActivity.kt
@@ -1,11 +1,35 @@
 package pl.mikom.drynomore.feature.nfc.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 
 class NfcRedirectActivity : ComponentActivity() {
 
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleNdefAction(intent)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        handleNdefAction(intent)
+        finish()
+    }
+
+    private fun handleNdefAction(intent: Intent) {
+        val nfcPayload = NfcUtil.readPayload(intent)
+        nfcPayload?.let {
+            Log.d(TAG, it)
+        }
+    }
+
+    companion object {
+
+        private val TAG = NfcRedirectActivity::class.java.simpleName
     }
 }
+
+
