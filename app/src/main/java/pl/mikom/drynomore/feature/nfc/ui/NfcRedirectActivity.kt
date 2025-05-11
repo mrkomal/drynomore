9c7eb24dfcdb63c1c087d4b68fb80407e6ea6b87
Michał <gry2@vp.pl>
2025-05-11 19:04:18 +0200

    Closes #4: Add NFC redirect activity

---
+++ app/src/main/java/pl/mikom/drynomore/feature/nfc/ui/NfcRedirectActivity.kt
@@ -0,0 +1,11 @@
+package pl.mikom.drynomore.feature.nfc.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class NfcRedirectActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
